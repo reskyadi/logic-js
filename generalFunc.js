f267f1c94ec0dbc3df72fef7be08090c796996c1
Resky Adi Nugraha <reskyadi@gmail.com>
2020-12-19 11:27:03 +0700

    Update general func

--- generalFunc.js
+++ generalFunc.js
@@ -55,3 +55,35 @@ console.log(str.toLowerCase());
 // Filter Obj
 const filterObj = arrObj.filter(item => item.name.toLowerCase().includes("nama 1"));
 console.log(filterObj);
+
+
+// JSON is a format for storing and transporting data.
+// JSON is often used when data is sent from a server to a web page.
+
+// What is JSON?
+// JSON stands for JavaScript Object Notation
+// JSON is a lightweight data interchange format
+// JSON is language independent *
+// JSON is "self-describing" and easy to understand
+
+
+// Stringify / ubah data ke string
+const objForString = {
+	id: 1,
+	name: 1
+};
+console.log("Convert to string", JSON.stringify(objForString));
+
+// JSON Parse / parsing data string ke JSON
+const stringForParse = "[1,2,3]";
+console.log("before parse", stringForParse);
+console.log("parsing string", JSON.parse(stringForParse));
+
+
+// Sort / Mengurutkan data
+const arrSort = [9, 8, 7, 6, 5, 4, 3, 2, 1, "c", "b", "a", "nama", ":~", "aa", "zz"];
+const sortArr = arrSort.sort();
+const sortArr2 = JSON.parse(JSON.stringify(sortArr));
+const reverseArr = sortArr2.reverse();
+console.log(sortArr);
+console.log(reverseArr);
