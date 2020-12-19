5afde1a3134d44154a8fdc2ab85bf44ea5b29ef9
Resky Adi Nugraha <reskyadi@gmail.com>
2020-12-19 09:49:20 +0700

    Init

---
+++ index.js
@@ -0,0 +1,11 @@
+// Inisialisasi var
+var a = "var a";
+a = "var a ubah";
+let b = "var b";
+b = "var b ubah";
+const c = "var c";
+// c = "var c ubah";
+
+console.log(a);
+console.log(b);
+console.log(c);
