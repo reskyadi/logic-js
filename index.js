fe9cdfc12637f49ddbc4cf358ae441cb9a197b30
Resky Adi Nugraha <reskyadi@gmail.com>
2020-12-19 09:52:59 +0700

    Add comment

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // Inisialisasi var
+// Tahun < 2015
 var a = "var a";
 a = "var a ubah";
+
+// Tahun > 2015 ES 6
+// Bisa ubah data var
 let b = "var b";
 b = "var b ubah";
+
+// Tidak bisa ubah data var
 const c = "var c";
-// c = "var c ubah";
 
 console.log(a);
 console.log(b);
