98339bf398db15471427d072f93f3ce636cc080a
Resky Adi Nugraha <reskyadi@gmail.com>
2020-12-19 10:05:04 +0700

    Add type

---
+++ typedata.js
@@ -0,0 +1,46 @@
+// Bool true / false bisa jua 0 / 1
+const booleanTrue = true;
+const booleanFalse = false;
+console.log(booleanTrue);
+console.log(booleanFalse);
+
+// Number
+// Type data float, integer
+// Float biasanya ada coma integer tidak
+const number = -1;
+const numberFloat = 0.112;
+console.log(number);
+console.log(numberFloat);
+
+// String
+const string = "Ini string";
+console.log(string);
+
+// Object, ibara benda memiliki apa saja, di buka dan di tutup { }
+const objectVar = {
+	id: 1,
+	name: "Ini Object Var"
+};
+console.log(objectVar);
+
+// Array, data variabel nya bisa banya dan di buka dan di tutup [ ]
+const arr = [1, 2, 3, "1", "2", "3"];
+console.log(arr);
+
+// Array of object, variabel yg memiliki banyak object
+const arrOfObject = [
+	{
+		id: 1,
+		name: "Ini Object Var"
+	},
+	{
+		id: 2,
+		name: "Ini Object Var"
+	}	
+];
+console.log(arrOfObject);
+
+const funcVar = () => {
+	return "Ini fungsi"
+};
+console.log(funcVar);
